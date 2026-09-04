cce500e6a5cd0eacd95b00331097779217a75b44
Aditya Yadav <217367477+adityayadav176@users.noreply.github.com>
2026-09-04 23:37:37 +0530

    implement screen share functionlity in server using sockets

--- src/socket/socket.server.js
+++ src/socket/socket.server.js
@@ -491,6 +491,49 @@ export const initializeSocket = (io) => {
                 })
             }
         })
+
+                // SCREEN SHARE ON
+
+        socket.on("screen_share_started", async (data) => {
+            try {
+                const { interviewRoomId } = data || {};
+
+                if (!interviewRoomId) {
+                    socket.emit("socket_error", {
+                        event: "screen_share_started",
+                        message: "interviewRoomID Required"
+                    })
+                }
+
+                const userId = socket.user._id;
+
+                const interview = await authorizeInterviewRoom({
+                    interviewRoomId,
+                    userId
+                })
+
+                const interviewRoom = `interview:${interview.interviewRoomId.toString()}`;
+
+                if (!socket.rooms.has(interviewRoom)) {
+                    socket.emit("socket_error", {
+                        event: "screen_share_started",
+                        message: "You have not joined this room"
+                    })
+                }
+
+                socket.to(interviewRoom).emit("remote_screen_share_started", {
+                    userId: userId.toString()
+                })
+
+            } catch (error) {
+                console.error("screen share error: ", error.message);
+
+                socket.emit("socket_error", {
+                    event: "screen_share_started",
+                    message: "Failed to start screen share"
+                })
+            }
+        })
         // JOIN CONVERSATION
 
         socket.on(
